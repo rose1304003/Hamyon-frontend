22e40f1af46071c48929f15a640cb54f936ad281
Rosie <axmedovarobiya16@gmail.com>
2026-01-16 16:14:45 +0500

    Add type definitions for Vite environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string
+  readonly VITE_BOT_USERNAME: string
+  // add more env variables here as needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
+
+
+
+
+
