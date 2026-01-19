77c91ba6314f5dfe2966be8f888aa5a25ec4e051
Rosie <axmedovarobiya16@gmail.com>
2026-01-19 11:29:59 +0500

    Add exports for analytics components

---
+++ src/components/analytics/index.ts
@@ -0,0 +1,5 @@
+export { WeeklyChart } from './WeeklyChart';
+export { MonthlyTrend } from './MonthlyTrend';
+export { SavingsProgress } from './SavingsProgress';
+export { SpendingAnalytics } from './SpendingAnalytics';
+export { AnalyticsView } from './AnalyticsView';
