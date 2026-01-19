69457262170b5fdfbe956f3b096198f056599247
Rosie <axmedovarobiya16@gmail.com>
2026-01-19 12:55:30 +0500

    Update index.ts

--- src/components/profile/index.ts
+++ src/components/profile/index.ts
@@ -1,4 +1,3 @@
-export { ProfileHeader } from './ProfileHeader';
-export { StatCard } from './StatCard';
-export { ProfileView } from './ProfileView';
-export { ProfileView } from './ProfileView';
+export { ProfileView } from "./ProfileView";
+export { ProfileHeader } from "./ProfileHeader";
+export { StatCard } from "./StatCard";
