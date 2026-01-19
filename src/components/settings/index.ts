2d8cb73c982b3a29a97dc8339bcabd31a07f74eb
Rosie <axmedovarobiya16@gmail.com>
2026-01-19 11:32:10 +0500

    Create index.ts

---
+++ src/components/settings/index.ts
@@ -0,0 +1,4 @@
+export { ThemeToggle } from './ThemeToggle';
+export { CurrencySettings, getCurrency, getCurrencySymbol } from './CurrencySettings';
+export { NotificationSettingsView } from './NotificationSettings';
+export { DataExport } from './DataExport';
